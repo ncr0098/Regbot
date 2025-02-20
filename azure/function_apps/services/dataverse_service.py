1f89932b6a3dbfb67f2217643f9ca506ffe08a8f
shinnosuke.matsuura <shinnosuke.matsuura@takeda.com>
2025-02-20 15:19:22 +0900

    delete unnecessary functon

--- azure/function_apps/services/dataverse_service.py
+++ azure/function_apps/services/dataverse_service.py
@@ -4,7 +4,7 @@ from msal_requests_auth.auth import ClientCredentialAuth
 from requests import Session
 
 class DataverseService:
-    def __init__(self, environment_url, entra_client_id, entra_client_secret, authority):
+    def __init__(self, environment_url, entra_client_id, entra_client_secret, authority, entity_logical_name):
         self.envieonment_url = environment_url
 
         app_reg = ConfidentialClientApplication(
@@ -23,3 +23,5 @@ class DataverseService:
 
         # Instantiate DataverseClient
         self.client = DataverseClient(session=session, environment_url=environment_url)
+        self.entity = self.client.entity(logical_name=entity_logical_name)
+    
