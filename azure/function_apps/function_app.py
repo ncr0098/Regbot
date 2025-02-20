fe94d56c5c021342d4d5cd63876e89cbe8664862
shinnosuke.matsuura <shinnosuke.matsuura@takeda.com>
2025-02-20 15:19:04 +0900

    more than 2 functions deployable

--- azure/function_apps/function_app.py
+++ azure/function_apps/function_app.py
@@ -4,6 +4,7 @@ import logging
 
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app1 = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="importFileToAISearch")
 def importFileToAISearch(req: func.HttpRequest) -> func.HttpResponse:
@@ -141,3 +142,24 @@ def importFileToAISearch(req: func.HttpRequest) -> func.HttpResponse:
             )
     except Exception as e:
         logging.error(f"Error occured: {e}")
+
+@app1.route(route="test_http_trigger")
+def test_http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
